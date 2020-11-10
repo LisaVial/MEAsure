c63399bdba0a348c393e0772d0473e1f8b199170
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2020-11-10 13:59:21 +0100

    * started to change SpikeDetection

--- spike_detection/spike_detection_settings.py
+++ spike_detection/spike_detection_settings.py
@@ -5,16 +5,22 @@ class SpikeDetectionSettings:
         TROUGHS = 1
         BOTH = 2
 
+    class ChannelSelection:
+        ALL = 0
+        SELECTION = 1
+
     def __init__(self):
         self.spike_window = 0.2  # in seconds
         self.mode = SpikeDetectionSettings.Mode.BOTH
         self.threshold_factor = 5
+        self.channel_selection = SpikeDetectionSettings.ChannelSelection.ALL
 
     def to_dict(self):
         result = dict()
         result["spike_window"] = self.spike_window
         result["mode"] = self.mode
         result["threshold_factor"] = self.threshold_factor
+        result["channel selection"] = self.channel_selection
         return result
 
     def from_dict(self, dictionary):
@@ -24,3 +30,5 @@ class SpikeDetectionSettings:
             self.mode = dictionary["mode"]
         if "threshold_factor" in dictionary.keys():
             self.threshold_factor = dictionary["threshold_factor"]
+        if "channel selection" in dictionary.keys():
+            self.channel_selection = dictionary["channel selection"]
